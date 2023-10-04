18afd62a7b126fb28875078e9aa7cc582a620c89
Vadim <vadim_kimm@icloud.com>
2023-10-04 19:26:13 +1100

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Vadim Kim on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
